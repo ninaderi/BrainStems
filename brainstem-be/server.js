5118a1c9e5191c2acdc1080518d8f64cd8b45d24
Seunjules <seun.mejule@gmail.com>
2019-07-05 16:21:21 -0600

    Server connecton update

--- brainstem-be/server.js
+++ brainstem-be/server.js
@@ -1,10 +1,13 @@
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('./db.sqlite');
-
-
-
+var cors = require('cors');
+const bodyParser = require('body-parser');
 const express = require('express');
-// const knex = require('knex');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
 var knex = require('knex')({
   client: 'sqlite3',
   connection: {
@@ -12,63 +15,66 @@ var knex = require('knex')({
   },
   useNullAsDefault: true
 });
-var cors = require('cors');
-const bodyParser = require('body-parser');
 
-const app = express();
-app.use(cors());
-app.use(bodyParser.json());
-console.log("Why aren't you working?")
 
-const users = [
-{
-	fname: "Seun",
-	lname: "Mejule",
-	password: ["1", "2" , "3"]
-},
-{
-	fname: "Nilou",
-	lname: "Naderi",
-	password: ["5", "6" , "7"]
-}, 
-]
+
+
+// *****************************THE TABLES IN KNEX IS INSTANTIATED BELOW FEEL FREE TO EDIT AND ADD NEW TABLES**************************
+
+knex.schema.createTable('student', (table) => {
+	table.increments('id');
+	table.string('fname');
+	table.string('lname');
+	table.string('title');
+	table.timestamps();
+  }).then(function () {
+	console.log('Movies Table is Created!')
+	.catch(err => console.log(err));
+  });
+
+
+  //****************************** SAMPLE INSERT STRING******************************************8
+//   knex('movies').insert({title: 'Slaughterhouse Fiveness'}).then(data => console.log(data))
+
+//   knex.select().table('movies')
+//   .then(data => console.log(data))
+
+
 
 app.get('/', (req, res) => {
-	console.log("Inside func")
 	res.send( "Hello World!")
 })
 
 app.post('/logIn', (req, res) => {
 	const {fname, lname, password} = req.body;
-	console.log(password);
-	if (this.state.fname != this.state.dbResponse.fname || this.state.lname != this.state.dbResponse.lname){
-			console.log("first loop")
-			this.setState({
-				logIn: false,
-				inputPassword: [],
-				fname: "",
-				lname: "",
-			})
-			return "Invalid credentials"
-		} else if(this.checkPassword(this.state.inputPassword, this.state.dbResponse.password) === false){
-			console.log("2nd loop", this.state.inputPassword, this.state.dbResponse.password)
-			this.setState({
-				logIn: false,
-				inputPassword: [],
-				fname: "",
-				lname: "",
-			})
-			return "Invalid password"
-		} else{
-			console.log("3rd loop")
-			this.setState({
-				logIn: true,
-				inputPassword: [],
-				fname: "",
-				lname: "",
-			})
-			return "You are in!"
-		}
+	// if (this.state.fname != this.state.dbResponse.fname || this.state.lname != this.state.dbResponse.lname){
+	// 		console.log("first loop")
+	// 		this.setState({
+	// 			logIn: false,
+	// 			inputPassword: [],
+	// 			fname: "",
+	// 			lname: "",
+	// 		})
+	// 		return "Invalid credentials"
+	// 	} else if(this.checkPassword(this.state.inputPassword, this.state.dbResponse.password) === false){
+	// 		console.log("2nd loop", this.state.inputPassword, this.state.dbResponse.password)
+	// 		this.setState({
+	// 			logIn: false,
+	// 			inputPassword: [],
+	// 			fname: "",
+	// 			lname: "",
+	// 		})
+	// 		return "Invalid password"
+	// 	} else{
+	// 		console.log("3rd loop")
+	// 		this.setState({
+	// 			logIn: true,
+	// 			inputPassword: [],
+	// 			fname: "",
+	// 			lname: "",
+	// 		})
+	// 		return "You are in!"
+	// 	}
 
 	console.log("Connecting!", req.body)
 	res.send({"reponse": "Hello World!"})
