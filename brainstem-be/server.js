05fff7d3922d9f90f2d9ebe0e15cdf29260f3332
Seunjules <seun.mejule@gmail.com>
2019-07-07 15:23:00 -0600

    Added tables to the database

--- brainstem-be/server.js
+++ brainstem-be/server.js
@@ -1,3 +1,7 @@
+// **************************THE SERVER IS BUILT ON knex.JS, THE DB FOR DEVELOPMENT IN SQLITE3 *****************************************************
+// ************************** DOCUMENTATION CAN BE FOUND AT knexjs.org******************************************************************************
+
+
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('./db.sqlite');
 var cors = require('cors');
@@ -19,18 +23,60 @@ var knex = require('knex')({
 
 
 
-// *****************************THE TABLES IN KNEX IS INSTANTIATED BELOW FEEL FREE TO EDIT AND ADD NEW TABLES**************************
+// *****************************THE TABLES IN KNEX ARE INSTANTIATED BELOW FEEL FREE TO EDIT AND ADD NEW TABLES**************************
 
 knex.schema.createTable('student', (table) => {
-	table.increments('id');
+	table.increments('id').primary();
+	table.specificType('stringarray', 'text ARRAY');
 	table.string('fname');
 	table.string('lname');
 	table.string('title');
+	table.string('wvUId');
 	table.timestamps();
+	table.unique('wvID');
   }).then(function () {
-	console.log('Movies Table is Created!')
-	.catch(err => console.log(err));
-  });
+	console.log('Student Table is Created!');
+  }).catch(err => console.log(err));
+
+  knex.schema.createTable('activity', (table) => {
+	table.increments('id').primary();
+	table.string('code');
+	table.string('wvname');
+	table.string('wvUId');
+	table.timestamps();
+	table.date("expiry");
+	table.boolean("status");
+	table.unique('code');
+  }).then(function () {
+	console.log('Activity Table is Created!');
+  }).catch(err => console.log(err));
+  
+  knex.schema.createTable('teacher', (table) => {
+	table.increments('id').primary();
+	table.string('fname');
+	table.string('wvID');
+	table.string('lname');
+	table.string('email');
+	table.timestamps();
+	table.unique('wvID');
+	table.unique('email')
+  }).then(function () {
+	console.log('Teacher Table is Created!');
+  }).catch(err => console.log(err));
+
+  knex.schema.createTable('roster', (table) => {
+	table.increments('id').primary();
+	table.string("rosterId")
+	table.string('studentName');
+	table.string('teacherName');
+	table.string('activityCode').references("code").inTable("activity");
+	table.integer('teacherID').references("id").inTable("teacher");
+	table.string('studentID').references("id").inTable("student");
+	table.timestamps();
+	table.unique("rosterId")
+  }).then(function () {
+	console.log('Roster Table is Created!');
+  }).catch(err => console.log(err));
 
 
   //****************************** SAMPLE INSERT STRING******************************************8
