da8b260e1e14e75675be0a8b5c800345cbd02574
Shane McGuire <shanelgmcguire@gmail.com>
2019-07-17 15:12:57 -0600

    added dashboard comp + router

--- brainstems-FE/src/App.js
+++ brainstems-FE/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import PicLogin from "./Components/picLogin";
+import DashboardComp from './Components/DashboardComp'
 
 function App() {
   return (
     <div className="App">
        <PicLogin />
+       <DashboardComp />
 
     </div>
   );
---
+++ brainstems-FE/src/Components/DashboardComp.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { NavLink, Switch, Route, Link, BrowserRouter as Router } from 'react-router-dom'
+
+class DashboardComp extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>hi from DashboardComp</h1>
+            </div>
+        )
+    }
+}
+
+export default DashboardComp
