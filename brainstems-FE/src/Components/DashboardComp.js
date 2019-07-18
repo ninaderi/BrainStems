d27e2a2e896f0508a209b86a7f9916dda9ba6bcc
Shane McGuire <shanelgmcguire@gmail.com>
2019-07-18 09:34:57 -0600

    added css layout, commented out studentlogin

--- brainstems-FE/src/Components/DashboardComp.js
+++ brainstems-FE/src/Components/DashboardComp.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import picLogin from './picLogin'
+//import picLogin from './picLogin'
 import AddActivityComp from './AddActivityComp'
 import ExistingActivitiesComp from './ExistingActivitiesComp'
 import CompletedActivitiesComp from './CompletedActivitiesComp'
@@ -11,22 +11,29 @@ class DashboardComp extends React.Component {
     render() {
         return (
             <Router>
-                <div>
-                    <h1>hi from DashboardComp</h1>
-                    <NavLink to='/studentlogin'>Student Login</NavLink>
-                    <NavLink to='/addactivity'>Add New Activity</NavLink>
-                    <NavLink to='/existingactivities'>Existing Activities</NavLink>
-                    <NavLink to='/completedactivities'>Completed Activities</NavLink>
-                    <NavLink to='/favouriteactivities'>Favourite Activities</NavLink>
-
+                <div id='container'>
+                    <header>
+                        <h1>Wonderville</h1>
+                    </header>                    
+                    <div id='navBar'>
+                        {/* <NavLink to='/studentlogin'>Student Login</NavLink> */}
+                        <ul>
+                            <li><NavLink to='/addactivity'>Add New Activity</NavLink></li>
+                            <li><NavLink to='/existingactivities'>Existing Activities</NavLink></li>
+                            <li><NavLink to='/completedactivities'>Completed Activities</NavLink></li>
+                            <li><NavLink to='/favouriteactivities'>Favourite Activities</NavLink></li>
+                        </ul>
+                    </div>
+                    <div id='bodyContainer'>
                     <Switch>
-                        <Route path='/studentlogin' component={picLogin} />
+                        {/* <Route path='/studentlogin' component={picLogin} /> */}
                         <Route path='/addactivity' component={AddActivityComp} />
                         <Route path='/existingactivities' component={ExistingActivitiesComp} />
                         <Route path='/completedactivities' component={CompletedActivitiesComp} />
                         <Route path='/favouriteactivities' component={FavouriteActivitiesComp} />
 
                     </Switch>
+                    </div>
                 </div>
             </Router>
         )
