5efd60fab80b780ad20bd81a1317025c842f8866
Greg Freson <greg.freson@gmail.com>
2019-07-18 13:16:49 -0600

    styling for dashboard

--- brainstems-FE/src/Components/DashboardComp.js
+++ brainstems-FE/src/Components/DashboardComp.js
@@ -4,24 +4,43 @@ import AddActivityComp from './AddActivityComp'
 import ExistingActivitiesComp from './ExistingActivitiesComp'
 import CompletedActivitiesComp from './CompletedActivitiesComp'
 import FavouriteActivitiesComp from './FavouriteActivitiesComp'
-
+import wonderville from "../images/wonderville.png";
 import { NavLink, Switch, Route, Link, BrowserRouter as Router } from 'react-router-dom'
 
+
+
 class DashboardComp extends React.Component {
+    constructor() {
+        super();
+        this.state = {};
+    };
+
+    // onOver = (e) => {
+    //     e.target.style.font_color = 'red';
+    //     e.target.style.transform = 'scale(1.3, 1.3)';
+        
+    // };
+
+    // onOut = (e) => {
+    //     // e.target.style.backgroundColor = '';
+    //     e.target.style.transform = 'initial';
+        
+    // };
+
     render() {
         return (
             <Router>
                 <div id='container'>
                     <header>
-                        <h1>Wonderville</h1>
+                      <img id="wvimg" src={wonderville} alt="wonderville" height="100px" />
                     </header>                    
                     <div id='navBar'>
                         {/* <NavLink to='/studentlogin'>Student Login</NavLink> */}
                         <ul>
-                            <li><NavLink to='/addactivity'>Add New Activity</NavLink></li>
-                            <li><NavLink to='/existingactivities'>Existing Activities</NavLink></li>
-                            <li><NavLink to='/completedactivities'>Completed Activities</NavLink></li>
-                            <li><NavLink to='/favouriteactivities'>Favourite Activities</NavLink></li>
+                            <li><NavLink className="menuitem" to='/addactivity'>ADD NEW ACTIVITY</NavLink></li>
+                            <li><NavLink className="menuitem" to='/existingactivities'>EXISTING ACTIVITIES</NavLink></li>
+                            <li><NavLink className="menuitem" to='/completedactivities'>COMPLETED ACTIVITIES</NavLink></li>
+                            <li><NavLink className="menuitem" to='/favouriteactivities'>FAVOURITE ACTIVITIES</NavLink></li>
                         </ul>
                     </div>
                     <div id='bodyContainer'>
