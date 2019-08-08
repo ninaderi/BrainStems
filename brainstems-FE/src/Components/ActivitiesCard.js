46a4166d609e07a276ea46a404706aedf30eaa89
Seunjules <seun.mejule@gmail.com>
2019-08-08 13:35:49 -0600

    updated activities card

--- brainstems-FE/src/Components/ActivitiesCard.js
+++ brainstems-FE/src/Components/ActivitiesCard.js
@@ -4,10 +4,25 @@ import vol from "../images/vol.jpg"
 
 class ActivitiesCard extends Component {
 
+
+constructor() {
+    super()
+    this.state = {
+        displayDetails: false,
+    }
+
+}
+handleClick = () => {
+    this.state.displayDetails 
+    ? this.setState({displayDetails: false})
+    : this.setState({displayDetails: true})
+}    
+
 render() {
 
     return (
         <div className = "activityDiv">
+        <div className = "topSection">
             <img id="activityImage" src = {vol} alt = "Picture of Activity" />
             <div className = "textField">
                 <table>
@@ -43,9 +58,48 @@ render() {
                 <h1>Activity Code</h1>
                 <p>{"props for activity code"}</p>
                 <button className = "button">Close Activity</button>
+                {this.state.displayDetails 
+                ?  <button className = "button" onClick = {this.handleClick}>Close Details</button>
+                :<button className = "button" onClick = {this.handleClick}>Details</button>
+                }
             </div>
+            </div>
+       {this.state.displayDetails ? 
+       
+       <div className = "detailsTable">
 
+        <h1>List of Students Assigned To this Activity</h1>
 
+        <table>
+                    <tbody>
+                        
+                        <tr>
+                            <th>Student Name</th>
+                            <th>Date Assigned</th>
+                            <th>Date Closed</th>
+                            <th>Progress</th>
+                            <th>More Info</th>
+                        </tr>
+                        <tr>
+                            <td>Student 1</td>
+                            <td>2019-06-01</td>
+                            <td>2019-06-03</td>
+                            <td>99%</td>
+                            <td>Info</td>
+                        </tr>
+                        <tr>
+                            <td>Student 2</td>
+                            <td>2019-06-01</td>
+                            <td>2019-06-03</td>
+                            <td>45%</td>
+                            <td>Info</td>
+                        </tr>
+                    </tbody>
+                </table>
+
+        </div>
+        : null
+       }
         </div>
     )
 
