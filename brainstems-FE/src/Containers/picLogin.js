c65a1937e3c64b887a6c98fc4bfa93512b729f11
Greg Freson <greg.freson@gmail.com>
2019-08-24 17:00:19 -0600

    changed and downsized piclogin pictures

--- brainstems-FE/src/Containers/picLogin.js
+++ brainstems-FE/src/Containers/picLogin.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react"
-import winter from "../images/winter.jpg"
-import worms from "../images/worms.jpg"
+import tennis from "../images/tennis.jpeg"
+import golf from "../images/golf.jpg"
 import volleyball from "../images/volleyball.jpg"
 import soccer from "../images/soccer.jpg"
-import gummies from "../images/gummies.jpg"
-import chocolate from "../images/chocolate.jpg"
-import forest from "../images/forest.jpg"
-import desert from "../images/desert.jpg"
+import hockey from "../images/hockey.jpg"
+import bowling from "../images/bowling.jpg"
+import football from "../images/football.jpeg"
 import basketball from "../images/basketball.jpg"
+import baseball from "../images/baseball.jpeg"
 import "../styles/picLogin.css"
 
 class PicLogin extends Component {
@@ -31,7 +31,7 @@ class PicLogin extends Component {
 
 	resetPad = () => {
 		this.inputPassword = ""
-		let list = ["soccer", "basketball", "volleyball", "desert", "winter", "forest", "gummies", "chocolate", "worms"]
+		let list = ["soccer", "basketball", "volleyball", "baseball", "tennis", "football", "hockey", "bowling", "golf"]
 		list.forEach(pixel => document.getElementById(pixel).className = "pixels")
 	}
 
@@ -46,12 +46,12 @@ render () {
 					<img src = {soccer} alt = "soccer" onClick={this.onClickPixels} className = "pixels" name = "soccer" id = "soccer" />
 					<img src = {basketball} alt = "basketball" onClick={this.onClickPixels} className = "pixels" name = "basketball" id = "basketball"/>
 					<img src = {volleyball} alt = "volleyball" onClick={this.onClickPixels} className = "pixels" name = "volleyball" id = "volleyball"/>
-					<img src = {desert} alt = "desert" onClick = {this.onClickPixels} className = "pixels" name = "desert" id = "desert"/>
-					<img src = {winter} alt = "winter" onClick = {this.onClickPixels} className = "pixels" name = "winter" id = "winter"/>
-					<img src = {forest} alt = "forest" onClick = {this.onClickPixels} className = "pixels" name = "forest" id = "forest"/>
-					<img src = {gummies} alt = "gummies" onClick = {this.onClickPixels} className = "pixels" name = "gummies" id = "gummies"/>
-					<img src = {chocolate} alt = "chocolate" onClick = {this.onClickPixels} className = "pixels" name = "chocolate" id = "chocolate"/>
-					<img src = {worms} alt = "worms" onClick = {this.onClickPixels} className = "pixels" name = "worms" id = "worms"/>
+					<img src = {baseball} alt = "baseball" onClick = {this.onClickPixels} className = "pixels" name = "baseball" id = "baseball"/>
+					<img src = {tennis} alt = "tennis" onClick = {this.onClickPixels} className = "pixels" name = "tennis" id = "tennis"/>
+					<img src = {football} alt = "football" onClick = {this.onClickPixels} className = "pixels" name = "football" id = "football"/>
+					<img src = {hockey} alt = "hockey" onClick = {this.onClickPixels} className = "pixels" name = "hockey" id = "hockey"/>
+					<img src = {bowling} alt = "bowling" onClick = {this.onClickPixels} className = "pixels" name = "bowling" id = "bowling"/>
+					<img src = {golf} alt = "golf" onClick = {this.onClickPixels} className = "pixels" name = "golf" id = "golf"/>
 				</div>
 				{this.props.buttonView === 'login'
 					? <button onClick={() => this.props.handleLogin(this.inputPassword)} className = "btn btn-md btn-accent"> Log In </button>
